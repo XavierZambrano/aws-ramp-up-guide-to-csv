973b67f504e338fcbb073d3e6b027c1908b2f781
Xavier Zambrano <xavierzamco@gmail.com>
2024-02-18 15:01:45 -0600

    feat: concatenate dataframes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import camelot
+import pandas as pd
 
 
 def remove_unnecessary_columns(table):
@@ -8,17 +9,30 @@ def remove_unnecessary_columns(table):
 def get_row_index_start_first_table(table):
     first_column = table.iloc[:, 0]
     index = (first_column == 'Learning Resource').idxmax()
-    return index - 1
+
+    return index - 1 if index > 0 else 0
 
 
-file = "input/Ramp-Up_Guide_Developer.pdf"
+file = 'input/Ramp-Up_Guide_Developer.pdf'
 
 # use a different table_areas for the first page?
 tables = camelot.read_pdf(file, flavor='stream', pages='all', edge_tol=50, row_tol=10)
 first_df = remove_unnecessary_columns(tables[0].df)
 index_start_first_table = get_row_index_start_first_table(first_df)
 first_df = first_df.iloc[index_start_first_table:]
-print(first_df)
+first_df.columns = ['Learning Resource', 'Duration (hrs)', 'Type']
+
+dfs = []
+dfs.append(first_df)
+for table in tables[1:]:
+    df = remove_unnecessary_columns(table.df)
+    df.columns = ['Learning Resource', 'Duration (hrs)', 'Type']
+    dfs.append(df)
+
+concatenated_df = pd.concat(dfs, axis=0, ignore_index=True)
+concatenated_df.to_csv('output.csv', index=False)
+print(concatenated_df)
+
 # tables.export('foo.csv', f='csv')  # json, excel, html
 
 
